a144ae86ae0a1100ccd6282784e2520201c738ab
fiyazbinhasan <fiyazhasan@live.com>
2016-05-24 01:16:46 +0600

    First Commit

---
+++ src/npm-bower-nuget-gulp/gulpfile.js
@@ -0,0 +1,16 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp'),
+    concat = require("gulp-concat"),
+    uglify = require("gulp-uglify");
+
+
+gulp.task("min:js", function () {
+    return gulp.src(["./wwwroot/app/**/*.js", "!" + "./wwwroot/app/**/*.min.js"], { base: "." })
+        .pipe(concat("./wwwroot/js/site.min.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest("."));
+});
---
+++ src/npm-bower-nuget-gulp/wwwroot/app/controller.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('controller', controller);
+
+    controller.$inject = [];
+
+    function controller() {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'controller';
+
+        activate();
+
+        function activate() { }
+    }
+})();
